fc7b420e25be6dee84237e506eb92a88c0898156
EC2 Default User <ec2-user@ip-10-0-1-125.ap-southeast-2.compute.internal>
2018-09-11 22:19:18 +0000

    fixed jinja template for sql

--- sql/query.sql
+++ sql/query.sql
@@ -1,3 +1,3 @@
 select distinct product_name from product where product_id IN 
   (select product_id from orderline where order_id 
-    IN (select order_id from order_info where customer_id='%(cust_id)'));
+    IN (select order_id from order_info where customer_id=%(cust_id)s));
