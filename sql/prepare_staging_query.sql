e8e2381c60bc6a005399a0fe08b7ca79fa5e6968
EC2 Default User <ec2-user@ip-10-0-1-125.ap-southeast-2.compute.internal>
2018-09-12 04:53:27 +0000

    new dag with s3 extract

---
+++ sql/prepare_staging_query.sql
@@ -0,0 +1,3 @@
+insert into staging_product (select distinct product_id, product_name from product where product_id IN
+      (select product_id from orderline where order_id
+        IN (select order_id from order_info where customer_id=%(cust_id)s)));
---
+++ stage_preprocess_s3.py
@@ -0,0 +1,43 @@
+from __future__ import print_function
+import airflow
+from airflow import DAG
+from acme.operators.dwh_operators import PostgresOperatorWithTemplatedParams
+from datetime import datetime, timedelta
+from airflow.models import Variable
+from airflow.operators import RedshiftToS3Transfer
+
+args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': airflow.utils.dates.days_ago(2)
+}
+
+tmpl_search_path = Variable.get("sql_path")
+
+dag = airflow.DAG(
+    'process_pipeline',
+    schedule_interval="* * * * *",
+    dagrun_timeout=timedelta(minutes=60),
+    template_searchpath=tmpl_search_path,
+    default_args=args,
+    max_active_runs=1)
+
+process_product_stg = PostgresOperatorWithTemplatedParams(
+    task_id='process_product_into_staging',
+    postgres_conn_id='orders_redshift',
+    sql='prepare_staging_query.sql',
+    parameters={"cust_id": "CUST-001"},
+    dag=dag,
+    pool='redshift_dwh')
+
+extract_product_stg = RedshiftToS3Transfer(
+    task_id='extract_product_stg_s3',
+    schema='orders',
+    table='staging_product',
+    s3_bucket='bigdataset-airflow',
+    s3_key='product'
+    redshift_conn_id='orders_redshift',
+    aws_conn_id='aws_conn_id',
+    include_header=True)
+
+process_product_stg >> extract_product_stg
